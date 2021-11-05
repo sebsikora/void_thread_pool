3591ffc06ad08777f1b5737d15cd1dcc0ef0d019
Sebastien Sikora <sebsikora@nidavellir.home>
2021-11-05 19:33:22 +0000

    Added to code comments.

--- void_thread_pool.hpp
+++ void_thread_pool.hpp
@@ -1,13 +1,32 @@
-// Ultra-simple thread pool for running void() functions in worker threads.
-// © 2021, Dr Seb Sikora.
-// seb.nf.sikora@protonmail.com
-//
-// A synthesis of the brilliantly helpful code snippets by users 'phd-ap-ece' and 'pio' on the following stackoverflow post -
-// 		https://stackoverflow.com/questions/15752659/thread-pooling-in-c11
-// Useful supplementary information found here -
-// 		https://stackoverflow.com/questions/10673585/start-thread-with-member-function
-// Explanation of the syntax [this]() { ... } for the predicate second argument to condition_variable.wait() found here -
-// 		https://stackoverflow.com/questions/39565218/c-condition-variable-wait-for-predicate-in-my-class-stdthread-unresolved-o
+/*
+	
+	void_thread_pool.hpp
+	
+	Copyright © 2021 Dr Seb N.F. Sikora.
+	
+	void_thread_pool.hpp is distributed under the terms of the MIT license.
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+	is furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+	IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	
+	seb.nf.sikora@protonmail.com
+	
+	* A synthesis of the brilliantly helpful code snippets by users 'phd-ap-ece' and 'pio' on the following stackoverflow post -
+	  https://stackoverflow.com/questions/15752659/thread-pooling-in-c11
+	* Useful supplementary information found here - https://stackoverflow.com/questions/10673585/start-thread-with-member-function
+	* Explanation of the syntax [this]() { ... } for the predicate second argument to condition_variable.wait() found here -
+	  https://stackoverflow.com/questions/39565218/c-condition-variable-wait-for-predicate-in-my-class-stdthread-unresolved-o
+	
+*/
 
 #ifndef VOID_TPOOL_H
 #define VOID_TPOOL_H
